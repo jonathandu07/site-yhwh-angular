9f790436c93a2089d1c8fd1e0f452ee51b2e3f99
jonathandu07 <alphajorider@gmail.com>
2022-04-19 21:16:28 +0200

    mise en place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
+import { FooterComponent } from './elements/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    PresentationComponent,
+    BlogComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
